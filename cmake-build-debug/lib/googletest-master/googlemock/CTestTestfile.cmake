406823f33c06cfffcf8829f4effa61a8ce27a8a8
dwiszyns <dominikwisza@gmail.com>
2019-05-02 22:43:37 +0200

    Changed some things from Pajak's solution to mine, still to need to fix more

--- cmake-build-debug/lib/googletest-master/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dominik/CLionProjects/Domino/lib/googletest-master/googlemock
-# Build directory: /home/dominik/CLionProjects/Domino/cmake-build-debug/lib/googletest-master/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
---
+++ src/Parser.h
@@ -0,0 +1,14 @@
+//
+// Created by dominik on 5/2/19.
+//
+
+#ifndef DOMINO_PARSER_H
+#define DOMINO_PARSER_H
+
+
+class Parser {
+
+};
+
+
+#endif //DOMINO_PARSER_H
